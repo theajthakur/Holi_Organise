fbfa03a0a496940d28cac6e144240a8ff63d050e
Vijay Singh <thakurvijayofficial@gmail.com>
2025-02-15 19:50:52 +0530

    Preparing

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
 const express = require("express");
+const path = require("path");
+
 const app = express();
 const port = 3000;
 
-app.get("/", (req, res) => res.send("Hello World!"));
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+app.set("view engine", "ejs");
+
+app.set("views", path.join(__dirname, "views"));
+
+app.use(express.static(path.join(__dirname, "public")));
+
+app.get("/", (req, res) => {
+  res.render("index");
+});
+
+app.listen(port, () => console.log(`App running on http://localhost:${port}`));
