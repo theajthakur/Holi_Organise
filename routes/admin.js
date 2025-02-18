dae64f5f812b36b8525565b39421e84c9b58b73d
Vijay Singh <thakurvijayofficial@gmail.com>
2025-02-19 00:13:46 +0530

    Admin Panel Updated

--- routes/admin.js
+++ routes/admin.js
@@ -3,11 +3,26 @@ const Router = express.Router();
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 const Admin = require("../models/Admin");
+const Pass = require("../models/Pass");
+const Payment = require("../models/Payment");
+const Referral = require("../models/Referral");
 
-Router.get("/", (req, res) => {
-  res.render("admin", {
-    user: req.user,
-  });
+Router.get("/", async (req, res) => {
+  try {
+    const users = await Pass.find({});
+    const payments = await Payment.find({});
+    const referral = await Referral.find({});
+    res.render("admin", {
+      user: req.user,
+      users: users,
+      payments: payments,
+      referral: referral,
+      payment: 0,
+    });
+  } catch (error) {
+    console.log(error);
+    return res.json({ status: "error", message: "Server Error!" });
+  }
 });
 
 // Router.post("/create", async (req, res) => {
@@ -36,5 +51,20 @@ Router.get("/", (req, res) => {
 //     res.status(500).json({ status: "error", message: "Server Error" });
 //   }
 // });
+
+Router.get("/api/dashboard", async (req, res) => {
+  try {
+    const users = await Pass.find({});
+    const referral = await Referral.find({});
+    res.json({
+      status: "success",
+      user: users,
+      referral: referral,
+    });
+  } catch (error) {
+    console.log(error);
+    return res.json({ status: "error", message: "Server Error!" });
+  }
+});
 
 module.exports = Router;
